9b293d8f14fa276afed2e123f2213d1366e5ebd2
Venkatbeemagari <114250014+Venkatbeemagari@users.noreply.github.com>
2022-10-22 21:46:13 +0530

    Create ASCII.java

---
+++ ASCII.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class ASCII{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    char a=sc.next().charAt(0);//it only reads the char
+    int b=(int)a;
+    System.out.println(b);
+  }
+}
